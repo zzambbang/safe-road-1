43bedf9b76a864dba493ad80eed94ae400b5dfc2
dlstodyo <dudgks3316@naver.com>
2021-08-20 01:13:20 +0900

    0820

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './styles.css';
 import SideBar from './sidebar/sidebar';
-import seoulmap from './seoulmap.jpg'
 import HeaderContainer from './components/Header';
+import { ReactComponent as Seoul } from './components/Seoulmap/Seoul.svg';
 
 function App() {
   return (
     <div id='App'>
+      <HeaderContainer />
       <SideBar pageWrapId={'page-wrap'} outerContainerId={'App'} />
-
       <div id='page-wrap'>
+        {/* br 문제 해결 */}
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         <h1>여성 아동 안심거리&지킴이집 길라잡이</h1>
-        <img alt='seoulmap' className='seoulmap' src={seoulmap} />
-
-        <HeaderContainer />
       </div>
+      <Seoul />
     </div>
   );
 }
