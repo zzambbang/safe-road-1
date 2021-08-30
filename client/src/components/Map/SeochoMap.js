ff2e8d420772119dfaaf3dfb19cf6cff0dd95359
dlstodyo <dudgks3316@naver.com>
2021-08-30 23:21:18 +0900

    new

--- client/src/components/Map/SeochoMap.js
+++ client/src/components/Map/SeochoMap.js
@@ -1,25 +1,34 @@
 /*global kakao*/ 
 import React, { useEffect } from 'react'
+import { SeochoData } from '../Marker/SeochoData';
 
-const SeochoMap=()=>{
+function SeochoMap() {
+  useEffect(() => {
+    mapscript();
+  }, []);
 
-  useEffect(()=>{
-    const container = document.getElementById('map');
-    const options = {
+  const mapscript = () => {
+    let container = document.getElementById("map");
+    let options = {
       center: new kakao.maps.LatLng(37.491042, 127.0068266),
-      level: 3
+      level: 5,
     };
 
+    //map
     const map = new kakao.maps.Map(container, options);
-    const markerPosition  = new kakao.maps.LatLng(37.491042,  127.0068266); 
-    const marker = new kakao.maps.Marker({
-      position: markerPosition
-  });
-  marker.setMap(map);
-
-    }, [])
-
-
+    
+    SeochoData.forEach((el) => {
+      // 마커 생성
+      new kakao.maps.Marker({
+        // 마커가 표시 될 지도
+        map: map,
+        // 마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        // 마커에 hover시 나타남
+        title: el.title,
+      });
+    });
+  };
     return (
         <div>
         <div id="map" style={{width:"800px", height:"800px"}}></div>
--- client/src/components/county/Gangnam_gu.js
+++ client/src/components/county/Gangnam_gu.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import GangnamMap from '../Map/GangnamMap'
 
 const Gangnam_gu = () => {
     return (
         <div>
-            <GangnamMap />
+            
         </div>
     )
 }
