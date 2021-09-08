5ee49d07d415bf9a8fdafe8f77b430b8152bff1f
dlstodyo <dudgks3316@naver.com>
2021-09-08 23:15:51 +0900

    Final

--- client/src/components/Howtouse/Howtouse.js
+++ client/src/components/Howtouse/Howtouse.js
@@ -1,11 +1,10 @@
 import React from "react";
-import './Carousel.css';
 import { Carousel } from "react-responsive-carousel";
 import manual1 from '../../images/manual1.png'
 import manual2 from '../../images/manual2.png'
 import manual3 from '../../images/manual3.png'
 
-export default () => (
+ const Howtouse = () => (
   <Carousel
     autoPlay="false"
     interval="2000"
@@ -16,13 +15,15 @@ export default () => (
     infiniteLoop="true"
   >
     <div>
-      <img alt='manual1' className='manual1' src={manual1} />  
+      <img alt='manual1' className='manual' src={manual1} />  
     </div>
     <div>
-      <img alt='manual2' className='manual2' src={manual2} />
+      <img alt='manual2' className='manual' src={manual2} />
     </div>
     <div>
-      <img alt='manual3' className='manual3' src={manual3} />
+      <img alt='manual3' className='manual' src={manual3} />
     </div>
   </Carousel>
 );
+
+export default Howtouse
